07cd635094018979e830aaaec1ea0026d16551b4
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-08-28 09:57:18 +0545

    feat: add category assets

---
+++ data/categoryData.ts
@@ -0,0 +1,80 @@
+export type Categories = {
+  id: number;
+  name: string;
+  slug: string;
+  image: string;
+  price: number;
+};
+
+export const categories: Categories[] = [
+  {
+    id: 1,
+    name: "Black Forest",
+    slug: "black-forest",
+    image: "/assets/category/blackForest/black_forest.jpg",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Chocolate",
+    slug: "chocolate",
+    image: "/assets/category/chocolate/1.jpeg",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "Cup Cakes",
+    slug: "cup-cakes",
+    image: "/assets/category/Cup Cakes/cup_cakes.jpg",
+    price: 100,
+  },
+  {
+    id: 4,
+    name: "Doll Cake",
+    slug: "doll-cake",
+    image: "/assets/category/doll_cake/1.jpg",
+    price: 100,
+  },
+  {
+    id: 5,
+    name: "Fruit Cake",
+    slug: "fruit-cake",
+    image: "/assets/category/Fruit Cake/1.jpg",
+    price: 100,
+  },
+  {
+    id: 6,
+    name: "Pastries",
+    slug: "pastries",
+    image: "/assets/category/pestries/pastries.jpg",
+    price: 100,
+  },
+  {
+    id: 7,
+    name: "White Forest",
+    slug: "white-forest",
+    image: "/assets/category/whiteForest/white_forest.jpg",
+    price: 100,
+  },
+  {
+    id: 8,
+    name: "Premium Cakes",
+    slug: "premium-cakes",
+    image: "/assets/category/Premium Cakes/1.jpeg",
+    price: 100,
+  },
+  {
+    id: 9,
+    name: "Red Velvet",
+    slug: "red-velvet",
+    image: "/assets/category/red valvet/3.jpeg",
+    price: 100,
+  },
+  {
+    id: 10,
+    name: "Vanilla",
+    slug: "vanilla",
+    image: "/assets/category/vanilla/5.jpeg",
+    price: 100,
+  },
+];
